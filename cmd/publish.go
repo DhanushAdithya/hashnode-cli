304c6ba46d28d54f379cadbb16c91cd00cf9672d
Dhanush Adithya <s.dhanushadithya@gmail.com>
2024-02-02 11:30:00 +0530

    feat(filepicker): publish cmd now has filepicker
    - added filepicker to select md file to post
    - added path flag to change the lookup directory

--- cmd/publish.go
+++ cmd/publish.go
@@ -12,9 +12,6 @@ var publishCmd = &cobra.Command{
 	Short: "Publish a post",
 	Run: func(cmd *cobra.Command, args []string) {
 		utils.CheckToken()
-		if viper.GetString("file") == "" {
-			utils.Exit("Please specify a file to publish (--file <path-to-file>)")
-		}
 		tui.Publish()
 	},
 }
@@ -27,16 +24,19 @@ func init() {
 	publishCmd.Flags().StringSliceP("tags", "T", []string{}, "Tags")
 	publishCmd.Flags().StringP("cover-image", "c", "", "Cover image")
 	publishCmd.Flags().StringP("publicationId", "p", "", "Publication to publish to")
+	publishCmd.Flags().StringP("path", "P", "", "Path to article markdown files")
 
 	viper.BindPFlag("file", publishCmd.Flags().Lookup("file"))
 	viper.BindPFlag("title", publishCmd.Flags().Lookup("title"))
 	viper.BindPFlag("tags", publishCmd.Flags().Lookup("tags"))
 	viper.BindPFlag("cover-image", publishCmd.Flags().Lookup("cover-image"))
 	viper.BindPFlag("publicationId", publishCmd.Flags().Lookup("publication"))
+	viper.BindPFlag("path", publishCmd.Flags().Lookup("path"))
 
 	viper.SetDefault("file", "")
 	viper.SetDefault("title", "")
 	viper.SetDefault("tags", []string{})
 	viper.SetDefault("cover-image", "")
 	viper.SetDefault("publicationId", "")
+	viper.SetDefault("path", ".")
 }
--- internal/tui/publish.go
+++ internal/tui/publish.go
@@ -3,39 +3,73 @@ package tui
 import (
 	"os"
 	"strings"
-	"sync"
 
 	"github.com/DhanushAdithya/hashnode-cli/internal/fetch"
 	"github.com/DhanushAdithya/hashnode-cli/internal/utils"
+	"github.com/charmbracelet/bubbles/filepicker"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/huh"
 	"github.com/spf13/viper"
 )
 
-func Publish() {
-	var (
-		title       string = viper.GetString("title")
-		tagsString  string = strings.Join(viper.GetStringSlice("tags"), ",")
-		cover       string = viper.GetString("cover-image")
-		publication string = viper.GetString("publicationId")
-	)
+var (
+	title       string
+	tagsString  string
+	cover       string
+	publication string
+	file        string
+	path        string
+	quit        bool
+)
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	response := make(chan struct{})
-	go func() {
-		defer wg.Done()
-		RenderLoad(response)
-	}()
+func loadFlags() {
+	title = viper.GetString("title")
+	tagsString = strings.Join(viper.GetStringSlice("tags"), ",")
+	cover = viper.GetString("cover-image")
+	publication = viper.GetString("publicationId")
+	file = viper.GetString("file")
+	path = viper.GetString("path")
+}
 
-	me := fetch.MeResponse()
-	if len(me.Errors) > 0 {
-		close(response)
-		wg.Wait()
-		utils.RenderAPIErrors(me.Errors)
+type fileModel struct {
+	filepicker.Model
+	selected bool
+}
+
+func (m fileModel) Init() tea.Cmd {
+	return m.Model.Init()
+}
+
+func (m fileModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	m.Model, cmd = m.Model.Update(msg)
+	if didSelect, path := m.Model.DidSelectFile(msg); didSelect {
+		file = path
+		m.selected = true
+		return m, tea.Quit
 	}
-	close(response)
-	wg.Wait()
 
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			quit = true
+			m.selected = true
+			return m, tea.Quit
+		}
+	}
+
+	return m, cmd
+}
+
+func (m fileModel) View() string {
+	if m.selected {
+		return ""
+	}
+	return m.Model.View()
+}
+
+func renderForm(me fetch.Me) {
 	var publications []huh.Option[string]
 
 	for _, pub := range me.Data.Me.Publications.Edges {
@@ -67,28 +101,69 @@ func Publish() {
 	if err := form.Run(); err != nil {
 		utils.Exit(err)
 	}
+}
 
+func postArticle() {
 	wg.Add(1)
-	response2 := make(chan struct{})
+	response := make(chan struct{})
 	go func() {
 		defer wg.Done()
-		RenderLoad(response2)
+		RenderLoad(response)
 	}()
 
-	content, err := os.ReadFile(viper.GetString("file"))
+	content, err := os.ReadFile(file)
 	if err != nil {
-		utils.Exit("Unable to read file:", viper.GetString("file"), "\nError:", err)
+		utils.Exit("Unable to read file:", file, "\nError:", err)
 	}
 	tags := strings.Split(tagsString, ",")
 
 	publish := fetch.PublishResponse(title, string(content), cover, publication, tags)
 	if len(publish.Errors) > 0 {
-		close(response2)
+		close(response)
 		wg.Wait()
 		utils.RenderAPIErrors(publish.Errors)
 	}
-	close(response2)
+	close(response)
 	wg.Wait()
 
 	utils.RenderSuccess("Post published successfully!")
+}
+
+func renderFilePicker() {
+	fp := filepicker.New()
+	fp.AllowedTypes = []string{"md"}
+	fp.CurrentDirectory = path
+
+	model := fileModel{Model: fp}
+	if _, err := tea.NewProgram(model).Run(); err != nil {
+		utils.Exit(err)
+	}
+}
+
+func Publish() {
+	wg.Add(1)
+	response := make(chan struct{})
+	go func() {
+		defer wg.Done()
+		RenderLoad(response)
+	}()
+
+	me := fetch.MeResponse()
+	if len(me.Errors) > 0 {
+		close(response)
+		wg.Wait()
+		utils.RenderAPIErrors(me.Errors)
+	}
+	close(response)
+	wg.Wait()
+
+	loadFlags()
+	renderForm(me)
+	if file == "" {
+		renderFilePicker()
+	}
+	if quit {
+		return
+	}
+	postArticle()
 }
